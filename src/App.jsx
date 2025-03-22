b51adf8d4ef4f8557ec3b30e717177e53b4a3e12
Samneang027 <154512847+Samneang027@users.noreply.github.com>
2025-03-22 17:01:04 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function MyApp() {
   return (
     <div>
-      <h1 className='bg-primary px-4 py-2 rounded'>Welcome to my app</h1>
+      <h1 className='bg-primary px-4 py-2 rounded'>Welcome to my app1</h1>
     </div>
   );
 }
