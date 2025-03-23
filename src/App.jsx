bc0ac41cb185742c25a48b67ad333bb632186eb1
Samneang027 <154512847+Samneang027@users.noreply.github.com>
2025-03-24 00:06:00 +0700

    add menu_phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
-
+import Menu_phone from './components/menu_phone';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function MyApp() {
   return (
     <div>
       <h1 className='bg-primary px-4 py-2 rounded'>Welcome to my app1</h1>
+      <Menu_phone/>
     </div>
   );
 }
